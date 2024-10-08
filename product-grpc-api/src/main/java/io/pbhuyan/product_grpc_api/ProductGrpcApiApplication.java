95d3cf6db51d468d268138f47406c225cdbf1fd0
Prasanta Bhuyan <prasanta.k.bhuyan@gmail.com>
2024-10-08 18:31:04 -0400

    Added GRPC api

---
+++ product-grpc-api/src/main/java/io/pbhuyan/product_grpc_api/ProductGrpcApiApplication.java
@@ -0,0 +1,13 @@
+package io.pbhuyan.product_grpc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductGrpcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductGrpcApiApplication.class, args);
+	}
+
+}
---
+++ product-grpc-api/src/test/java/io/pbhuyan/product_grpc_api/ProductGrpcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pbhuyan.product_grpc_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductGrpcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
