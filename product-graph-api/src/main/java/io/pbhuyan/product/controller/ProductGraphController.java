e2fdcaea909e42eae3fce4a83f07f7f1a8c11b6f
Prasanta Bhuyan <prasanta.k.bhuyan@gmail.com>
2024-10-15 08:47:57 -0400

    Secured graph controller with isAuthenticated()

--- product-graph-api/src/main/java/io/pbhuyan/product/controller/ProductGraphController.java
+++ product-graph-api/src/main/java/io/pbhuyan/product/controller/ProductGraphController.java
@@ -8,8 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -17,6 +17,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/")
 @RequiredArgsConstructor
+@PreAuthorize("isAuthenticated()")
 public class ProductGraphController {
     private final ProductService productService;
 
